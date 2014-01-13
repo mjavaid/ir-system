70f03a79f9518b2aef9f5f51718e53d27404a957
Muhammad Javaid <mjavaid@cisco.com>
2014-01-13 13:41:08 -0500

    Added populateDocuments util function that populates the documents into a python dict from the corpus file

--- applibs/utils.py
+++ applibs/utils.py
@@ -4,3 +4,36 @@
 # Last Modified: 1/12/14
 """
 
+DOCUMENTS_FILE = "../resources/Trec_microblog11.txt"
+TWEET_ID = 0
+
+""" GLOBAL SYSTEM VARIABLES """
+
+STOPWORD_LIST = [
+    "the", "a", "be", "it"
+]
+DOCUMENTS = []
+
+""" END GLOBAL SYSTEM VARIABLES """
+
+""" GLOBAL SYSTEM FUNCTIONS """
+
+### populateDocuments
+# Reads the corpus file and populates the DOCUMENTS list in the IR system
+# which will be used for querying.
+###
+def populateDocuments():
+    input = open(DOCUMENTS_FILE, "r")
+    docs = (input.read()).split("\n")
+    i = 0
+    list = []
+    for doc in docs:
+        docInfo = doc.split()
+        if docInfo:
+            DOCUMENTS.append({"D"+str(i):{"id": docInfo[TWEET_ID], "text": docInfo[TWEET_ID+1:]}})
+            i += 1
+
+""" END GLOBAL SYSTEM FUNCTIONS """
+
+if __name__ == "__main__":
+    populateDocuments()
