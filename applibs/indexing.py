0d60f3f9c9edeb36d929eac61720b3f06a70cd6e
Rayan Alsubhi <khyal2008@gmail.com>
2014-01-14 15:31:04 -0500

    Added capabilty to pass an array of words and index them. Fixed an issue that causes the doc list to have duplicate entries(i.e D0: 1, D0: 1) while it should be (D0: 2)

--- applibs/indexing.py
+++ applibs/indexing.py
@@ -8,28 +8,32 @@
 TABLE_LIST={}
 
 ### addToTable
-# add the token to the table and update df and the linked list
+# add the tokens from the token array to the table and update df and the linked list
 # param: docNum, token
 ###
-def addToTable(docNum,token):
-    # Check if the token is already in the list
-    if token in TABLE_LIST:
-        # If True, increment df
-        TABLE_LIST[token]['df']+=1
-        # Check if the document number is already in the list
-        if TABLE_LIST[token]['doc'] == docNum:
-            # If yes, increment the occurence 
-            TABLE_LIST[token]['doc'][docNum]+=1
+def addToTable(docNum,tokenArr):
+    for token in tokenArr:  
+        # Check if the token is already in the list
+        if token in TABLE_LIST:
+            # If True, increment df
+            TABLE_LIST[token]['df']+=1
+            # Check if the document number is already in the list
+            
+            index = 0
+            for i in TABLE_LIST[token]['doc']:
+                if docNum in i:
+                    # increment the tf
+                    TABLE_LIST[token]['doc'][index][docNum]+=1
+                else:
+                    # If not, add the document number to the list and initialize it to 1
+                    TABLE_LIST[token]['doc'].append({docNum:1})
+                    break
+                index+=1
         else:
-            # If not, add the document number to the list and initialize it to 1
-            TABLE_LIST[token]['doc'].append({docNum:1})
-    else:
-        # If Token is not in the dictionary, add it and initialize both df and occurence to 1
-        TABLE_LIST[token]={'df':1,'doc':[{docNum :1}]}
+            # If Token is not in the dictionary, add it and initialize both df and occurence to 1
+            TABLE_LIST[token]={'df':1,'doc':[{docNum :1}]}
         
 if __name__ == "__main__":
-    addToTable('D0','game')
-    addToTable('D1','game')
-    addToTable('D0','great')
-    print(TABLE_LIST)
-    print(TABLE_LIST['game']['df'])
+    addToTable('D0',['game','bad','bad'])
+    addToTable('D1',['game','home','bad'])
+    for token in TABLE_LIST: print(token, " >>", TABLE_LIST[token])
